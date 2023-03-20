ded1f7311f0a0dac0f50687dd4ff5980551a0deb
Fabricio <fabriciodev@outlook.com>
2023-03-20 14:57:42 -0300

    Enviando exercicio

---
+++ ExercicioListas/Models/Employees.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioListas
+{
+  public class Employees
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Salary { get; private set; }
+
+    public Employees(int id, string name, double salary)
+    {
+      Id = id;
+      Name = name;
+      Salary = salary;
+    }
+
+    public void increaseSalary(double percentage)
+    {
+      Salary += Salary * (percentage / 100);
+    }
+
+    public override string ToString()
+    {
+      return Id +
+              ", "
+              + Name +
+              ", "
+              + Salary +
+              ".";
+    }
+  }
+}
---
+++ ExercicioListas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using ExercicioListas;
+
+List<Employees> employees = new List<Employees>();
+
+Console.Write("How many employees will be registered? ");
+int n = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < n; i++)
+{
+  Console.Write("\nId: ");
+  int id = int.Parse(Console.ReadLine());
+
+  Console.Write("Name: ");
+  string name = Console.ReadLine();
+
+  Console.Write("Salary: ");
+  double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+  employees.Add(new Employees(id, name, salary));
+}
+
+Console.Write("Enter the employee id that will have salary increase: ");
+int idSearch = int.Parse(Console.ReadLine());
+
+Employees validationId = employees.Find(x => x.Id == idSearch);
+if (validationId != null)
+{
+  int index = employees.FindIndex(x => x.Id == idSearch);
+  Console.Write("Enter the percentage: ");
+  double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+  employees[index].increaseSalary(percentage);
+}
+else
+{
+  Console.WriteLine($"ID {idSearch} DOES NOT EXIST!");
+}
+
+Console.WriteLine("\nUpdated list of employees:");
+
+foreach (Employees obj in employees)
+{
+  Console.WriteLine(obj.ToString());
+}
