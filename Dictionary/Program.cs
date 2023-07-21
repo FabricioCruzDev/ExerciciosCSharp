d102e4d73897b246f2637c26cc509158c3ac8c50
Fabricio <fabriciodev@outlook.com>
2023-07-20 23:39:26 -0300

    Adicionei o exercício Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+Dictionary<string, int> ballotCount = new Dictionary<string, int>();
+
+Console.WriteLine("Enter file full path: ");
+string path = Console.ReadLine();
+
+try
+{
+  using (StreamReader rs = new StreamReader(path))
+  {
+    while (!rs.EndOfStream)
+    {
+      string[] line = rs.ReadLine().Split(",");
+      if (!(ballotCount.ContainsKey(line[0])))
+      {
+        ballotCount[line[0]] = int.Parse(line[1]);
+      } else ballotCount[line[0]] += int.Parse(line[1]);
+    }
+
+    Console.WriteLine("RESULT");
+
+    foreach (KeyValuePair<string, int> item in ballotCount)
+    {
+      Console.WriteLine(item.Key + ": " + item.Value);
+    }
+  }
+}
+catch (IOException e)
+{
+  Console.WriteLine("File error!", e.Message);
+}
---
+++ Dictionary/README.md
@@ -0,0 +1,30 @@
+# Exercício proposto - Dictionary
+
+Na contagem de votosde uma eleição, são gerados vários registros devotação contendo o nome do candidato e aquantidade de votos(formato .csv) que ele obteve em uma urna de votação.
+
+- Você deve fazer um programa para ler os registros de votação a partir de um arquivo, daígerar um relatório consolidado com os totais decada candidato.
+
+## Dados
+
+- Alex Blue,15
+- Maria Green,22
+- Bob Brown,21
+- Alex Blue,30
+- Bob Brown,15
+- Maria Green,27
+- Maria Green,22
+- Bob Brown,25
+- Alex Blue,31
+- Input file example:
+
+## Exemplo
+
+### Enter file full path: c:\temp\in.txt
+
+## Resultado
+
+### Alex Blue: 76
+
+### Maria Green: 71
+
+### Bob Brown: 61
